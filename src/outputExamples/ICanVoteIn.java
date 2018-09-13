be78ead757e74e41b7a7ce94cf8e2c7476086ebe
Adam  Negron <negron.adam.a@gmail.com>
2018-09-13 13:23:21 -0500

    YearsUntilYouCanVote

---
+++ src/outputExamples/ICanVoteIn.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class ICanVoteIn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I can vote in 1 year");
+	}
+
+}
