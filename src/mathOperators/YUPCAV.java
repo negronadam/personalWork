3d3d1618b54b4a2867246f8324f0f0536b5999c8
Adam  Negron <negron.adam.a@gmail.com>
2018-09-21 14:42:59 -0500

    Math Operators Practice

---
+++ src/mathOperators/YUPCAV.java
@@ -0,0 +1,16 @@
+package mathOperators;
+import java.util.Scanner;
+public class YUPCAV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age;
+		int votingAge;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter your age");
+		age = input.nextInt();
+		votingAge = 18 - age;
+		System.out.println("You can vote in: " + votingAge + " Years!");
+	}
+
+}
