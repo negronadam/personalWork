90735be0ce676447e2ad4f2c6d102099441ec10c
Adam  Negron <negron.adam.a@gmail.com>
2018-09-24 14:49:15 -0500

    LongDivision

--- src/inputExamples/LongDivision.java
+++ src/inputExamples/LongDivision.java
@@ -1,10 +1,19 @@
 package inputExamples;
-
+import java.util.Scanner;
 public class LongDivision {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int dividend;
+		int divisor;
+		int remainder;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the divisor");
+		divisor = input.nextInt();
+		System.out.println("Please enter the dividend");
+		dividend = input.nextInt();
+		remainder = dividend % divisor;
+		System.out.println("The remainder from the divison is: " + remainder);
 	}
 
 }
