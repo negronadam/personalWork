c748bdde850eb3129e0a1d3ecca3a73096952f6d
Adam  Negron <negron.adam.a@gmail.com>
2018-10-01 14:25:38 -0500

    OOP

---
+++ src/objectOrientedProgramming/Bob.java
@@ -0,0 +1,11 @@
+package objectOrientedProgramming;
+
+public class Bob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter Bob = new Greeter();
+		Bob.sayHello();
+	}
+
+}
--- src/objectOrientedProgramming/Greeter.java
+++ src/objectOrientedProgramming/Greeter.java
@@ -7,4 +7,8 @@ public class Greeter {
 		
 	}
 	
+	public void sayHello()
+	{
+		System.out.println("Hello!");
+	}
 }
