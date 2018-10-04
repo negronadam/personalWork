40061cb1baf571b19918ea7f08a2b9fa76009547
Adam  Negron <negron.adam.a@gmail.com>
2018-10-04 14:09:55 -0500

    Loads of OOP Stuff

---
+++ src/objectOrientedProgramming/NumbersDef.java
@@ -0,0 +1,9 @@
+package objectOrientedProgramming;
+
+public class NumbersDef {
+	public void sayNumberplus2(double num1, double num2)
+	{
+		System.out.println("Your number plus 2 is: " + num1 + 2 + num2);
+	}	
+}
+
---
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -0,0 +1,10 @@
+package objectOrientedProgramming;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
