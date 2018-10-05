020f05bf1fde3c5bbd77b11099c87005b08049f1
Adam  Negron <negron.adam.a@gmail.com>
2018-10-05 15:03:13 -0500

    Lots of cool OOP stuff

--- src/objectOrientedProgramming/NumbersTest.java
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -4,7 +4,9 @@ public class NumbersTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		NumbersDef bill = new NumbersDef();
+		bill.returnRoundUp(1.2);
+		
 	}
 
 }
