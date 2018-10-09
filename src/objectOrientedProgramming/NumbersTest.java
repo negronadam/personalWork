1f7b87920d09ce41433c08c631879a50a5816b52
Adam  Negron <negron.adam.a@gmail.com>
2018-10-09 15:04:40 -0500

    InstanceFields

--- src/objectOrientedProgramming/NumbersTest.java
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -5,7 +5,7 @@ public class NumbersTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		NumbersDef bill = new NumbersDef();
-		bill.returnRoundUp(1.2);
+		bill.returnRoundUp(7.4);
 		
 	}
 
---
+++ src/objectOrientedProgramming/TestClassOOPStuff.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+import java.util.Scanner;
+public class TestClassOOPStuff {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		MoreOOPStuff run = new MoreOOPStuff();
+		run.getAge();
+		System.out.println(run.getAge());
+		
+	}
+
+}
