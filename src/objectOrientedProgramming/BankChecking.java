d4593667f9499a004278e09175b7f1c35a3864c4
Laptop <negron.adam.a@gmail.com>
2018-10-10 09:18:38 -0500

    ProgrammingOppBank

---
+++ src/objectOrientedProgramming/BankChecking.java
@@ -0,0 +1,18 @@
+package objectOrientedProgramming;
+
+public class BankChecking {
+
+	private double checkingAmount;
+	
+	public BankChecking()
+	{
+		checkingAmount = 530.00;
+	}
+	
+	public double getChecking()
+	{
+		return checkingAmount;
+	}
+	
+	
+}
