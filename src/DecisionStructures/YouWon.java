cae0f2288e7cd3342b1fc3246ef3fccacc324c4f
Unknown <negron.adam.a@gmail.com>
2018-10-18 14:10:21 -0500

    Fixes To Decision Structures

--- src/DecisionStructures/YouWon.java
+++ src/DecisionStructures/YouWon.java
@@ -5,10 +5,10 @@ public class YouWon {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		double money = -999.0;
-		double epsilon = 0.001;
+		double epsilon = 0.01;
 		System.out.println("How much money did you recieve for your fifth birthday?");
 		money = input.nextDouble();
-		if (Math.abs(money-Math.sqrt(84.3))<= epsilon)
+		if (Math.abs(money-Math.sqrt(84.3)) < epsilon)
 		{
 			System.out.println("You won!");
 		}
