8f0dd8a89492bb0a89cc290fcbe677d16e388181
Unknown <negron.adam.a@gmail.com>
2018-10-23 15:06:26 -0500

    Other Cool Stuff

--- src/DecisionStructures/Timer.java
+++ src/DecisionStructures/Timer.java
@@ -5,11 +5,26 @@ public class Timer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
+		double count = 0;
+		double num1 = 0;
+		double num2 = 0;
+		double num3 = 0;
+		int stopat = 0;
+		double countToQuit = 0;
+		double average = 0;
+		while (num1 != 9999)
 		{ 
-			
+			System.out.println("What is your number?");
+			countToQuit = input.nextInt();
+			/*
+			System.out.println(count);
+			count = count+=2;
+			int newercount = stopat + stopat;
+			System.out.println("The sum of your numbers is: " + newercount);
+			*/
 		}
-		
+		double sum = count + count;
+		System.out.println(sum);
 	}
 
 }
