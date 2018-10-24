a56e97027ef30d76b69c833f9174124261bdb7bb
Unknown <negron.adam.a@gmail.com>
2018-10-24 15:08:14 -0500

    ForgotToSaveChanges

--- src/DecisionStructures/DoWhile.java
+++ src/DecisionStructures/DoWhile.java
@@ -6,13 +6,22 @@ public class DoWhile {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int num = 0;
+		/*
 		do
 		{
 			System.out.println("The magic number is: " + (num * 17) + "!");
 			System.out.println("Please enter a number! Hint: Enter 9999 to escape purgatory.");
 			num = input.nextInt();
 		}while(num != 9999);
+		*/
 		
+		do
+		{
+			
+			System.out.println("Enter your number");
+			num = input.nextInt();
+			
+		}while(num != 9999);
 	}
 
 }
