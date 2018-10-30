e983e87296e0e3fc47b579412eda106386b41f62
Unknown <negron.adam.a@gmail.com>
2018-10-30 14:08:53 -0500

    MoreLoops

---
+++ src/DecisionStructures/Calculator.java
@@ -0,0 +1,46 @@
+package DecisionStructures;
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int option = 0;
+		System.out.println("Welcome to the calculator!");
+		System.out.println("1. Is It ARight Triangle");
+		System.out.println("2. Power");
+		System.out.println("3. Square Root");
+		System.out.println("4. Round Up");
+		System.out.println("5. Round Down");
+		System.out.println("6. Quit");
+		switch(option)
+		{
+			case 1:
+				
+				System.out.println("You have chosen Is It A Right Triangle!");
+				System.out.println("Please enter the base of the triangle");
+			break;
+			
+			case 2:
+				
+			break;
+			
+			case 3:
+				
+			break;
+				
+			case 4:
+				
+			break;
+				
+			case 5:
+				
+			break;
+				
+			case 6:
+				
+			break;
+		}
+	}
+
+}
---
+++ src/DecisionStructures/NestedLoops.java
@@ -0,0 +1,40 @@
+package DecisionStructures;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+			/*
+	}
+		for (int i = 0; i <= 4; i++)
+		{
+			for(int j = 0; j <= 5; j++)
+			{
+				System.out.println(" " + i * j + " ");
+				
+				
+				
+				
+			}System.out.println(" ");
+			
+			*/
+			
+			
+			/*The Process:
+			 * Find the integer that has the most amount of divisors that have no remainder
+			 * 
+			 * 
+			 * 
+			 * 
+			 */
+			
+			for (int j = 1; j <= 60; j++)
+			{
+				if(60 % j == 0);
+				{
+					System.out.println(j);
+				}
+		}
+
+	}
+
+}
