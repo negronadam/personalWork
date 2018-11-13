df677b9916a6d6ccdb6b8b923c0971aa0f47440d
Adam <negron.adam.a@gmail.com>
2018-11-13 15:04:53 -0600

    Substrings, ActivitytoLower, Length

---
+++ src/Strings/ActivitySubstring.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class ActivitySubstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "My name is Buzz Lightyear!";
+		
+		for (int i = 0; i < text.length(); i++)
+		{
+			System.out.println(text.substring(i));
+		}
+	}
+
+}
--- src/Strings/Length.java
+++ src/Strings/Length.java
@@ -5,10 +5,18 @@ public class Length {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		String test1 = "Test";
+		String test1 = "test";
+		/*
 		System.out.println("Please enter a string");
 		test1 = input.nextLine();
 		System.out.println("The string is " + test1.length() + " characters long");
+		*/
+		System.out.println("Please type a sentance");
+		test1 = input.nextLine();
+		for(char index = 0; index < test1.length(); index++)
+		{
+			System.out.println(test1.charAt(index));
+		}
 	}
 
 }
