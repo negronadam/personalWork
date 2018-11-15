5c432feedd1ddbc946679c6fc7ce8ed1d1303a86
Adam <negron.adam.a@gmail.com>
2018-11-15 13:36:18 -0600

    EssayStart

---
+++ src/Strings/Essay.java
@@ -0,0 +1,66 @@
+package Strings;
+
+public class Essay {
+	private String authorsNameFirst;
+	private String authorsNameSecond;
+	private String authorsNameThird;
+	private String essayText;
+	public Essay()
+	{
+		String authorsNameFirst = "test";
+		String authorsNameSecond = "test";
+		String authorsNameThird = "test";
+		String essayText = "test";
+	}
+	
+	public void setAuthorsName(String authorsNameFirstX, String authorsNameSecondX )
+	{
+		authorsNameFirst.equals(authorsNameFirstX);
+		authorsNameThird.equals(authorsNameSecondX);
+	}
+	
+	public String getFirstName()
+	{
+		return authorsNameFirst;
+	}
+	
+	public String getLastName()
+	{
+		return authorsNameSecond;
+	}
+	
+	public String setText(String TestX)
+	{
+		essayText = TestX;
+		return TestX;
+	}
+	
+	public String getLastNameCaps()
+	{
+		return authorsNameThird.toUpperCase();
+	}
+	
+	public int getFirstNameLength()
+	{
+		return authorsNameFirst.length();
+	}
+	
+	public Boolean findLetterX()
+	{
+		return authorsNameFirst.contains("X") || authorsNameFirst.contains("x") || authorsNameSecond.contains("X") || authorsNameFirst.contains("x");
+	}
+	
+	public int getWordCount()
+	{
+		int words = 0;
+		for(int i = 0; i < essayText.length(); i++)
+		{
+			if (essayText.charAt(i) == ' ')
+			{
+				words++;
+			}
+		}
+		return words;
+	}
+	
+}
