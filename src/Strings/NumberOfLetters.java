5b6fa2904a9e8907adb5558f9484205907dcdf1d
Unknown <negron.adam.a@gmail.com>
2018-11-15 13:52:09 -0600

    NumberOfLetters

---
+++ src/Strings/NumberOfLetters.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.util.Scanner;
+public class NumberOfLetters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String test = "Test";
+		System.out.println("Please enter a sentence.");
+	}
+
+}
