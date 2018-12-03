401774c6b06bb44bfd2d0b7510d8ff5d59ae8d70
Unknown <negron.adam.a@gmail.com>
2018-12-03 08:51:39 -0600

    more stuff

--- src/Arrays/RentACar.java
+++ src/Arrays/RentACar.java
@@ -1,10 +1,26 @@
 package Arrays;
-
+import java.util.Scanner;
+import java.util.Random;
 public class RentACar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		Random generator = new Random();
+		int carsOwned = -999;
+		System.out.println("How many cars does your company own?");
+		carsOwned = input.nextInt();
+		int[] cars = new int[carsOwned];
+		String[] carMake = new String[carsOwned];
+		for(int i = 0; i < cars.length; i++)
+		{	
+			System.out.println("What is the make of your car?");
+			carMake[i] = input.nextLine();
+		}
+		for(int a = 0; a < cars.length; a++)
+		{
+			System.out.println(carMake[a]); 
+		}
 	}
 
 }
