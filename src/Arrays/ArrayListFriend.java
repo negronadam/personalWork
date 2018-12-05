dc0bd4d6d5da44bdd9e9cf4dbdfa89cd3622a6c8
Adam <negron.adam.a@gmail.com>
2018-12-05 14:52:45 -0600

    Stuff

---
+++ src/Arrays/ArrayListFriend.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayListFriend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Friend> friend = new ArrayList<Friend>();
+		Scanner input = new Scanner(System.in);
+		for(int i = 0; i < 3; i++)
+		{
+			System.out.println("Please enter the name and age of a friend");
+			friend.add(0, (new Friend(input.nextLine(), input.nextInt())));
+		}
+		
+		for(int a = 0; a < friend.size(); a++)
+		{
+			System.out.println(friend.get(a));
+		}
+	}
+
+}
