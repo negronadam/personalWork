13b62a75b9eadb126629a722c1f615483636f485
Adam <negron.adam.a@gmail.com>
2018-12-12 15:08:19 -0600

    Testting my CompSci Test

---
+++ src/Arrays/ArrayListCheck.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class ArrayListCheck {
+ private String name;
+ private String phone;
+ public ArrayListCheck(String xName, String xPhone)
+ {
+	 name = xName;
+	 phone = xPhone;
+ }
+ 
+ public String getName()
+ {
+	 return name;
+ }
+}
