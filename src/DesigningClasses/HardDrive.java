948f3cedb8449be69eb3db0fdc4426cc5cd02ea9
Adam <negron.adam.a@gmail.com>
2018-12-20 13:15:41 -0600

    Working on HardDrive

--- src/DesigningClasses/HardDrive.java
+++ src/DesigningClasses/HardDrive.java
@@ -1,8 +1,37 @@
 package DesigningClasses;
-
+import java.util.ArrayList;
 public class HardDrive {
+	private String Name;
+	private double totalCapacity;
+	private double freeSpace;
+	ArrayList<Double> files = new ArrayList<Double>();
 	public HardDrive()
 	{
 		
 	}
+	
+	public void addFile(double xSize)
+	{
+		if(xSize < freeSpace)
+		{
+			freeSpace -= xSize;
+			files.add(xSize);
+		}
+		
+		else
+		{
+			System.out.println("There is not enough room on the Hard Drive!");
+		}
+	}
+	
+	public void deleteFile(int xIndex)
+	{
+		Double fileToDelete = files.get(xIndex);
+		freeSpace += fileToDelete;
+		files.remove(xIndex);
+	}
+	
+	
+	
+	
 }
