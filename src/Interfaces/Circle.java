ddb2baa867f5206d41568dbc5306354228c14adc
Adam <negron.adam.a@gmail.com>
2019-01-03 14:08:09 -0600

    Downloaded all teacher resources. Created first interface too

---
+++ src/Interfaces/Circle.java
@@ -0,0 +1,30 @@
+package Interfaces;
+
+public class Circle {
+private double radius;
+    
+    public Circle()
+    {
+	radius = 10;
+    }
+    public Circle(double r)
+    {
+         radius = r;
+    }
+    public double getRadius()
+    {
+	return radius;
+    }
+    public void setRadius(double r)
+    {
+	radius = r;
+    }
+    public double perimeter()
+    {
+        return (2.0*Math.PI*radius);
+    }
+    public double area()
+    {
+        return (Math.PI*(radius*radius));
+    }
+}
